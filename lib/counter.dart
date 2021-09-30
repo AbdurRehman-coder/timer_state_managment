5c4c1c5795b221818afa600071c4852833b199d9
Abdur Rehman <abdurrehmanlkl786@gmail.com>
2021-10-01 01:26:22 +0500

    add providerNotifier to the  app root

--- lib/counter.dart
+++ lib/counter.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:provider/provider.dart';
 
 class Counter with ChangeNotifier{
+  //counter var that will increment
+  int _counter = 0;
+  // getter to access private _counter
+  int get counter => _counter;
+
+  // method that will increment the counter
+  // and also set the state through NotifyListener
+ void addCounter(){
+   _counter++;
+   //this listener works like setState
+   notifyListeners();
+ }
+
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:timer_state_managment/counter.dart';
 import 'package:timer_state_managment/home.dart';
 
 void main() {
@@ -11,8 +13,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-     home: HomeScreen(),
-    );
+    // Add provider at the root of the App
+    // it will listen for all changes that will
+    // occur inside the app
+   return ChangeNotifierProvider(
+       create: (context) => Counter(),
+     child: Builder(
+       builder: (BuildContext context){
+         return const MaterialApp(
+           home: HomeScreen(),
+         );
+       },
+     ),
+   );
   }
 }
